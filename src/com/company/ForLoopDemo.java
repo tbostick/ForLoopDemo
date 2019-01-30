fe8f6321a7f44f7ef2c3a29241bae5ccb87281d9
tbostick <timbostick1950@gmail.com>
2019-01-30 16:16:52 -0500

    Initial commit

---
+++ src/com/company/ForLoopDemo.java
@@ -0,0 +1,19 @@
+package com.company;
+
+public class ForLoopDemo
+{
+
+
+    public static void main(String[] args) {
+        // for loop begins when x=2
+        // and runs till x <10
+        int x;
+        for (x = 2; x < 10; x++) {
+
+            System.out.println("Running value of x:" + x);
+
+        }
+        System.out.println("Final value of x:" + x);
+    }
+}
+
